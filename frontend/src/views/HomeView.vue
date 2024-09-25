c5113aea0fe6898de2ac1677b5a4aab29ab7a6f3
Buger404 <ris_vb@126.com>
2024-09-25 21:40:57 +0800

    feat: 导航栏和首页基础样式

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,6 +4,26 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <p class="title">2024 巴黎奥运会</p>
   </main>
 </template>
+
+<style scoped>
+.title {
+  display: flex;
+  width: 827px;
+  height: 171px;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  text-align: center;
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: 20px; /* 31.25% */
+  letter-spacing: 0.1px;
+  background: linear-gradient(273deg, #000 -26.29%, #F2BA71 84.49%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
