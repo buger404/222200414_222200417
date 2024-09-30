30833fbb6c4ee46a5346245906db46e3796dcf8d
Buger404 <ris_vb@126.com>
2024-09-30 15:32:24 +0800

    fix: nginx 错误

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,7 @@ package main
 import "github.com/cloudwego/hertz/pkg/app/server"
 
 func main() {
-	h := server.Default(server.WithHostPorts("127.0.0.1:6563"))
+	h := server.Default(server.WithHostPorts("0.0.0.0:6563"))
 
 	register(h)
 	h.Spin()
